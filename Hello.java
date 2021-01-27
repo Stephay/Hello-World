38078fc38ee398ed00aae8e71d62092f53e87a7d
Stephay <78101997+Stephay@users.noreply.github.com>
2021-01-27 13:00:15 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*****************************************************************
+*Hello World
+******************************************************************
+*Stephanie Osei
+*01/27/2021
+*CSMC255 003
+******************************************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+		System.out.println("<Stephanie Osei>");
+		System.out.println("<HelloWorld>");
+		System.out.println("<HelloWorld.Java>");
+		System.out.println("<01/27/2021>");
+		System.out.println("<CSMC255 003>");
+	}
+}
